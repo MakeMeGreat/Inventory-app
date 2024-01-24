60287e1df4ab2abb14820d3de60f85605fa67e63
Azamat <zamametshin@gmail.com>
2024-01-24 18:39:54 +0300

    second codelab is completed

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -1,13 +1,18 @@
 package com.example.inventory
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.inventory.data.Item
 import com.example.inventory.data.ItemDao
 import kotlinx.coroutines.launch
+import java.text.NumberFormat
 
-class InventoryViewModel(private val itemDao: ItemDao): ViewModel() {
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+
+    val allItems: LiveData<List<Item>> = itemDao.getItems().asLiveData()
 
     private fun insertItem(item: Item) {
         viewModelScope.launch {
@@ -15,7 +20,7 @@ class InventoryViewModel(private val itemDao: ItemDao): ViewModel() {
         }
     }
 
-    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String):Item {
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
         return Item(
             itemName = itemName,
             itemPrice = itemPrice.toDouble(),
@@ -28,19 +33,63 @@ class InventoryViewModel(private val itemDao: ItemDao): ViewModel() {
         insertItem(newItem)
     }
 
+    private fun getUpdatedItemEntry(
+        id: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ): Item {
+        val price = itemPrice.replace(',','.')
+        return Item(id, itemName, price.toDouble(), itemCount.toInt())
+    }
+
+    fun updateItem(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ) {
+        val updateItem = getUpdatedItemEntry(itemId, itemName, itemPrice, itemCount)
+        updateItem(updateItem)
+    }
+
     fun isEntryValid(itemName: String, itemPrice: String, itemCount: String): Boolean {
-        if (itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank())
-            return false
-        return true
+        return !(itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank())
+    }
+
+    fun retrieveItem(id: Int): LiveData<Item> {
+        return itemDao.getItem(id).asLiveData()
+    }
+
+    private fun updateItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.update(item)
+        }
+    }
+
+    fun sellItem(item: Item) {
+        if (item.quantityInStock > 0) {
+            val newItem = item.copy(quantityInStock = item.quantityInStock - 1)
+            updateItem(newItem)
+        }
+    }
+
+    fun isStockAvailable(item: Item): Boolean {
+        return item.quantityInStock > 0
+    }
+
+    fun deleteItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.delete(item)
+        }
     }
 }
 
-class InventoryViewModelFactory(private val itemDao: ItemDao): ViewModelProvider.Factory {
+class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         @Suppress("UNCHECKED_CAST")
         if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
             return InventoryViewModel(itemDao) as T
-        }
-        else throw IllegalStateException("Unknown ViewModel class")
+        } else throw IllegalStateException("Unknown ViewModel class")
     }
 }
---
+++ app/src/main/java/com/example/inventory/ItemListAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.inventory
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.inventory.data.Item
+import com.example.inventory.data.getFormattedPrice
+import com.example.inventory.databinding.ItemListItemBinding
+
+class ItemListAdapter(private val onItemClicked: (Item) -> Unit) :
+    ListAdapter<Item, ItemListAdapter.ItemListViewHolder>(DiffCallback) {
+
+    class ItemListViewHolder(private var binding: ItemListItemBinding) : ViewHolder(binding.root) {
+        fun bind(item: Item) {
+            binding.itemName.text = item.itemName
+            binding.itemPrice.text = item.getFormattedPrice()
+            binding.itemQuantity.text = item.quantityInStock.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ItemListAdapter.ItemListViewHolder {
+        return ItemListViewHolder(
+            ItemListItemBinding.inflate(
+                LayoutInflater.from(parent.context)
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemListAdapter.ItemListViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
+        }
+        holder.bind(current)
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Item>() {
+            override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem.itemName == newItem.itemName
+            }
+        }
+    }
+}
